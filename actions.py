b904fc130d404a84abe6a293b6d74a818306bf62
Cosmin Poieana <cosmin@robocorp.com>
2024-05-09 13:11:35 +0400

    Fix docstrings/code format and update deps (#6)
    * Fix docstrings/code format and update deps
    
    * Action refactoring
    
    * Tweaks
    
    Deps updates:
    - python=3.10.14 #to get patch fixes
    - uv=0.1.41 #0.1.42 not available for windows atm
    
    ---------
    
    Co-authored-by: Kari Harju <kari@robocorp.com>

--- actions.py
+++ actions.py
@@ -1,29 +1,29 @@
-"""
-A simple AI Action template for comparing timezones
+"""A simple AI Action template for comparing timezones.
 
 Please checkout the base guidance on AI Actions in our main repository readme:
-https://github.com/sema4ai/actions/blob/master/README.md
-
+https://github.com/sema4ai/actions#readme
 """
 
 from datetime import datetime
-from zoneinfo import ZoneInfo
 
 import pytz
 from sema4ai.actions import action
+from zoneinfo import ZoneInfo
 
 
 @action
 def compare_time_zones(user_timezone: str, compare_to_timezones: str) -> str:
-    """
-    Compares user timezone time difference to given timezones
+    """Compares user timezone time difference to given timezones.
 
     Args:
-        user_timezone (str): User timezone in tz database format. Example: "Europe/Helsinki"
-        compare_to_timezones (str): Comma seperated timezones in tz database format. Example: "America/New_York, Asia/Kolkata"
+        user_timezone: User timezone in tz database format.
+            Example: "Europe/Helsinki"
+        compare_to_timezones: Comma separated timezones in tz database format.
+            Example: "America/New_York, Asia/Kolkata"
 
     Returns:
-        str: List of requested timezones, their current time and the user time difference in hours
+        A string with the requested timezones, their current time and the user time
+        difference in hours.
     """
     output: list[str] = []
 
@@ -31,7 +31,7 @@ def compare_time_zones(user_timezone: str, compare_to_timezones: str) -> str:
         user_tz = pytz.timezone(user_timezone)
         user_now = datetime.now(user_tz)
     except pytz.InvalidTimeError:
-        return f"Timezone '{user_timezone}' could not be found. Use tz database format."
+        return f"Timezone {user_timezone!r} could not be found. Use tz database format."
 
     output.append(
         f"- Current time in {user_timezone} is {user_now.strftime('%I:%M %p')}"
@@ -57,6 +57,6 @@ def compare_time_zones(user_timezone: str, compare_to_timezones: str) -> str:
             )
 
     # Pretty print for log
-    print("\n".join(output))
-
-    return "\n".join(output)
+    output_string = "\n".join(output)
+    print(output_string)
+    return output_string
